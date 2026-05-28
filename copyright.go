121c5d6ad45177096e0f95084d5d02874dfc9ae9
Brian W <45476717+bw19@users.noreply.github.com>
2026-05-27 18:52:12 -0700

    Update LICENSE and copyright

--- copyright.go
+++ copyright.go
@@ -1,5 +1,5 @@
 /*
-Copyright 2023 Microbus LLC and various contributors
+Copyright 2023-yyyy Microbus LLC and various contributors
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
@@ -14,7 +14,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-//go:generate go run github.com/microbus-io/copyrighter -r
+//go:generate go run github.com/microbus-io/copyrighter
 
 package quickhct
 
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/microbus-io/quickhct
 
 go 1.20
 
-require github.com/microbus-io/copyrighter v0.2.0
+require github.com/microbus-io/copyrighter v1.3.0
